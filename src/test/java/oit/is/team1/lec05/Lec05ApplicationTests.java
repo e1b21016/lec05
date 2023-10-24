cde8fe5e1196719d87d5a7d475f7cae67a2d87db
e1b21016 <e1b21016@oit.ac.jp>
2023-10-24 14:05:44 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/team1/lec05/Lec05ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team1.lec05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lec05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
